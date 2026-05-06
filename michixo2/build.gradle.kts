b7990f38c765fc1387ca383106464ad7255a658b
Luis Gonzalez <luisgnzhdz@gmail.com>
2026-05-05 20:20:29 -0600

    chore(release): bump mobile app version to 1.4

--- michixo2/build.gradle.kts
+++ michixo2/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.luigidev.michixo.mobile"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
